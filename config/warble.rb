4c5da17ba4c73dbe31a67d9e316fe094c9b63454
Joe Kutner <jpkutner@gmail.com>
2015-04-29 15:30:55 -0500

    Updated for warbler

--- config/warble.rb
+++ config/warble.rb
@@ -1,4 +1,8 @@
 Warbler::Config.new do |config|
   config.features = %w(executable)
-  config.jar_name = "jruby-async-servlets-example"
+  config.jar_name = "heroku-warbler-example"
+  config.excludes = FileList["vendor/**/*"]
+  if ENV['STACK'] == "cedar-14"
+    config.gem_path = "WEB-INF/vendor/bundle"
+  end
 end
